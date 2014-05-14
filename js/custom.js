90143decd4c989c7b4dbf9e2d3c5af80a7508c9b
unmeshweboniselab <unmesh.dusane@weboniselab.com>
2014-05-14 17:25:22 +0530

    change made in html and css

--- js/custom.js
+++ js/custom.js
@@ -2,4 +2,6 @@ $(document).ready(function(){
     var docHeight = $(window).height();
     var docWidth = $(window).width();
    $(".wrapper > div").css("height",docHeight);
+
+    var s = skrollr.init();
 });
